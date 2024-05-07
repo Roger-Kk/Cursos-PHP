1864ee2832ea3fab739ecae9a5cd856d8d76760f
Roger-Kk <rogerkolossoski@gmail.com>
2024-05-06 23:22:16 -0300

    Finalização Curso 2

--- PHP Criando sua aplicação/desafios.php
+++ PHP Criando sua aplicação/desafios.php
@@ -17,3 +17,57 @@ $temperaturaEmFahrenheit = $temperaturaEmCelsius * 1.8 + 32;
 $mensagem = "A temperatura de $temperaturaEmCelsius Celsius é equivalente a $temperaturaEmFahrenheit Fahrenheit";
 
 echo $mensagem;
+
+//Conta Bancária
+$saldo = 1_000; //o "_" em um número
+$titularConta = 'Roger K';
+
+echo "\n\n***********************\n";
+echo "Titular: $titularConta\n";
+echo "Saldo atual: R$ $saldo\n";
+echo "***********************\n";
+
+do{
+
+    echo "1. Consultar saldo atual\n";
+    echo "2. Sacar valor\n";
+    echo "3. Depositar valor\n";
+    echo "4. Sair\n";
+    echo "************************\n"; 
+
+    $opcao = (int) fgets(STDIN);
+
+    switch ($opcao){
+        case 1:
+            echo "\n\n***********************\n";
+            echo "Titular: $titularConta\n";
+            echo "Saldo atual: R$ $saldo\n";
+            echo "***********************\n";
+            break;
+
+        case 2:
+            echo "Qual valor deseja sacar?\n";
+            $valorSacar = (float) fgets(STDIN);
+            if ($valorSacar > $saldo){
+                echo "Saldo Insuficiente.\n";
+            } else {
+                $saldo -= $valorSacar;
+            }
+            break;
+
+        case 3:
+            echo "Quanto deseja depositar?\n";
+            $valorDepositar = (float) fgets(STDIN);
+            $saldo += $valorDepositar;
+            break;
+
+        case 4: 
+            echo "Adeus.";
+            break;
+
+        default:
+            echo "Opção inválida.";
+        break;
+    }
+} while ($opcao != 4);
+
--- PHP Criando sua aplicação/screenMatch.php
+++ PHP Criando sua aplicação/screenMatch.php
@@ -1,6 +1,4 @@
-
 <?php
-
 
 //OPERADORES
 $a = 10; // Atribui o valor 10 à variável $a
@@ -18,23 +16,113 @@ $menorIgual = $b <= $c; // A variável $menorIgual ficará com o valor *true*, p
 echo "Bem vindo ao Screen Match";
 echo "\n";
 
-$nomeFilme = "Top Gun";
+$nomeFilme = "Top Gun - Maverick";
+$nomeFilme = "Se beber não case";
+$nomeFilme = "Thor-Ragnarok";
 $anoLancamento = $argv[1]  ?? 2022; //o $argv contém todos os valores passados pela linha de comando.
-$somaNotas = 9 + 6 + 8 + 7.5 + 5;
-$notaFilme = $somaNotas/5;
+$notas = [];
+
+//Estrutura de LOOP: FOR
+
+for ($contador = 1; $contador < $argc; $contador ++) {
+    $notas[] = (float) $argv[$contador];
+};
+
+//ForEach
+foreach ($notas as $nota){
+    $somaNotas += $nota;
+}
+
+
+
+//WHILE
+/*
+$contador = 1;
+while($argv[$contador] != 0){
+    $somaNotas += $argv[$contador++];
+};
+*/
+
+//DO WHILE
+/*
+do {
+
+} while (){
+
+}
+*/
+
+$quantidadeNotas = $argc - 1; //o $arg contém o índice do valor passados pela linha de comando
+$notaFilme = array_sum($notas)/$quantidadeNotas;
 $planoPrime = true;
 $incluinoPlano = $planoPrime || $anoLancamento < 2020;
 
 echo "Nome do filme: " . $nomeFilme . "\n"; //Aspas duplas
-echo 'Nome do filme: ' . $nomeFilme . '\n'; //Aspas simples
-echo "Ano de lançamento: ". $anoLancamento . "\n"; 
+//echo 'Nome do filme: ' . $nomeFilme . '\n'; //Aspas simples
+echo "Ano de lançamento: ".$anoLancamento . "\n"; 
+echo "Nota do filme: " . $notaFilme ."\n";
 
+//Clausula IF
+if ($anoLancamento > 2022) {
+    echo "Esse filme é um lançamento";
+} else if($anoLancamento > 2020 && $anoLancamento <= 2022){
+    echo "Esse filme ainda é novo.";
+}else {
+    echo "Esse filme não é um lançamento\n";
+}
 
+//Match Expression
+$genero = match ($nomeFilme){
+    "Top Gun - Maverick" => "ação",
+    "Thor-Ragnarok" => "super-herói",
+    "Se beber não case" => "comédia",
+    default => "desconhecido",
+};
+echo "O gênero do filme é ".$genero;
 
+//Array
+$notasParaOFilme = [
+    10,
+    9,
+    8,
+    7,
+    6.5,
+    4
+];
 
+var_dump($notasParaOFilme);
 
+//Array associativo
+$filme = [
+    "nome" => "Thor - Ragnarok",
+    "ano" => 2021,
+    "nota" => 7.8,
+    "genero" => "super-herói",
+];
+echo $filme["nome"]."\n";
+echo $filme["nota"]."\n";
 
+//Switch Case
+/*
+switch (expressão) {
+    case valor1:
+       // código a ser executado se a expressão for igual a valor1
+       break;
+    case valor2:
+       // código a ser executado se a expressão for igual a valor2
+       break;
+    case valor3:
+       // código a ser executado se a expressão for igual a valor3
+       break;
+    default:
+       // código a ser executado se a expressão não for igual a nenhum valor
+       break;
+ }
+ */
 
-
+ //Operador ternário
+ //$resultado = $expressao1 ? $expressao2 : $expressao3;
+//Se $expressao1 for verdadeira (true), $resultado terá o valor 
+//de $expressão2. Caso contrário, $resultado terá o valor de $expressão3.
 
 
