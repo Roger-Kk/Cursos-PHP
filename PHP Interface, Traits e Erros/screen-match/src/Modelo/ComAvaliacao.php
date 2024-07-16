e081413a4264cdda901111d103500871914af6a2
Roger <rogerkolossoski@gmail.com>
2024-07-16 00:50:20 -0300

    Add files via upload

--- PHP Interface, Traits e Erros/screen-match/src/Modelo/ComAvaliacao.php
+++ PHP Interface, Traits e Erros/screen-match/src/Modelo/ComAvaliacao.php
@@ -1,16 +1,25 @@
 <?php
 
 namespace ScreenMatch\Modelo;
+use ScreenMatch\Exception\NotaInvalidaException;
 
 //"Características", ou Traits ou Herança Horizontal
 //Permite que outras classes usem dos métodos, porém sem ser um tipo, ou ainda
-//sem permitir que seja haja outras classes estendidas dela.
+//sem permitir que haja outras classes estendidas dela.
 trait ComAvaliacao
 {
     private array $notas = [];
 
+    //Ao comentar da seguinte forma, quando se chama o método e passa o mouse em cima, mostra o comentário.
+    /**
+     * @throws \InvalidArgumentException Se a nota for negativa ou maior que 10
+     */
     public function avalia(float $nota): void
-    {
+    {   
+        //Tratamento de erros: Caso a nota seja negativa ou > 10 gera um exceção
+        if($nota < 0 || $nota > 0){
+            throw new NotaInvalidaException();
+        }
         $this->notas[] = $nota;
     }
 
@@ -24,4 +33,3 @@ trait ComAvaliacao
 
 
 }
-
