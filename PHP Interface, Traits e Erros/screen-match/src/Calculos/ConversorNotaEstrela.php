38b98ed670bb9a9cf6cc3ab0dd181c34809a8497
Roger <rogerkolossoski@gmail.com>
2024-07-16 00:49:44 -0300

    Add files via upload

--- PHP Interface, Traits e Erros/screen-match/src/Calculos/ConversorNotaEstrela.php
+++ PHP Interface, Traits e Erros/screen-match/src/Calculos/ConversorNotaEstrela.php
@@ -3,15 +3,32 @@
 namespace ScreenMatch\Calculos;
 
 use ScreenMatch\Modelo\Avaliavel;
+use ArgumentCountError;
+use DivisionByZeroError;
+use Throwable;
 //É possível dar um apelido para o diretório do arquivo:
 //use ScreenMatch\Modelo\Avaliavel as AvaliavelTrait;
 
 class ConversorNotaEstrela
 {
     public function converte(Avaliavel $avaliavel): float{
+
+    //Tratamento de Erros: 
+    try{
         $nota = $avaliavel->media();
 
         //Realiza a conversão
         return  round($nota) / 2;
+        } catch(DivisionByZeroError){
+            return 0;
+        }catch(ArgumentCountError $e){
+            echo $e->getMessage();
+            return 0;
+        } catch(Throwable){
+            return 0;
+        }
     }
+    //Try{ }catch(){}: se dentro do try, acontecer um erro do tipo DivisionByZeroError, executa a ação do catch
+    //OU se tiver um erro do tipo ArgumntCountError, exibe a mensagem de erro e retorna 0
+    //Ou qualquer que seja o erro, retorne 0;
 }
