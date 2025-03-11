9d66ef684f0dfbb17edac6656c8fd42afa49f396
Roger-Kk <rogerkolossoski@gmail.com>
2025-03-10 22:29:03 -0300

    arrumando código

--- PHP Padrão MVC/cria-usuario.php
+++ PHP Padrão MVC/cria-usuario.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -20,32 +19,3 @@ $statement = $pdo->prepare($sql);
 $statement->bindValue(1,$email);
 $statement->bindValue(2,$hash);
 $statement->execute();
-
-
-
-=======
-<?php
-
-declare(strict_types=1);
-
-$host = 'localhost';
-$dbname = 'padrao_mvc';
-$username = 'root';
-$password = '';
-
-$pdo = new PDO("mysql:host = $host; dbname = $dbname", $username, $password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$email = $argv[1];
-$password = $argv[2];
-$hash = password_hash($password, PASSWORD_ARGON2ID);
-
-$sql = 'INSERT INTO padrao_mvc.users (email, password)  VALUES (?, ?);';
-$statement = $pdo->prepare($sql);
-$statement->bindValue(1,$email);
-$statement->bindValue(2,$hash);
-$statement->execute();
-
-
-
->>>>>>> 0641de1614d0790b43b701fbad6295a7628803c3
