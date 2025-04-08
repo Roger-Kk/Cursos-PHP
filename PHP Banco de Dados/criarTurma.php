03b4fc3558504281ebfbcb512b3e7e4973a536d8
Roger-Kk <rogerkolossoski@gmail.com>
2025-04-08 09:54:51 -0300

    Final curso

--- PHP Banco de Dados/criarTurma.php
+++ PHP Banco de Dados/criarTurma.php
@@ -10,7 +10,27 @@ $studentRepository = new PdoStudentRepository($connection);
 
 //Definição da turma
 
+//beginTransaction é um método que vai armazenando as queries;
+$connection->beginTransaction();
 
+$aStudent = new Student (
+    null,
+    'Nico Timba',
+    new DateTimeImmutable('1985-03-04')
+);
+$studentRepository->save($aStudent);
+$anotherStudent = new Student(
+    null,
+    'Robalo Master',
+    new DateTimeImmutable('1960-15-09')
+);
+$studentRepository->save($anotherStudent);
 
+$connection->commit();
+
+//Caso queira cancelar o que foi feito na transação após o beginTransaction, pode-se usar o rollBack() para desfazer.
+//$connection->rollBack();
 
 //Inserir os alunos da turma
+
+//ATENÇÃO: CURSO FINALIZADO NA AULA 5. Não foi escrito o código de tratamento de erros das aulas 6 e 7. 
